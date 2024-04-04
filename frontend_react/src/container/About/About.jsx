c1ee1afb052cec990c342dba41c890f0b32b8039
Emediong David Titus <emedattitus24@gmail.com>
2024-04-04 20:08:07 +0100

    corrected ...

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
 
   return (
         <>
-        <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
+        <h2 className="head-text">Beyond the UI: <span>Design Thinking</span> <br />for a Seamless <span> Developer Experience</span></h2>
 
       <div className="app__profiles">
             {abouts.map((about, index) => (
